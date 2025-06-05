4e2e680389762e1e8887e8feef84fa4ec5e12a23
katy.trop <etropkina@yandex.ru>
2025-06-05 16:40:49 +0300

    main commit

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import './modules/ibg.js';
-import './modules/isWebp.js';
-//import './modules/burger.js';
-//import './modules/functions.js';
-//import './modules/spollers.js';
-//import './modules/sliders.js';
-//import './modules/dynamic-adapt.js';
